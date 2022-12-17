3831039a56c379dbd0c46dd9431be01263793e50
dschom <dschomburg@mozilla.com>
2022-12-16 17:52:49 -0800

    fix(content): Fix buggy timing metrics
    Because:
    - We could see in Sentry that various times provided to metrics endpoint were consistently off.
    
    This Commit:
    - Creates an abstraction around the performance API to isolate some of its complexities.
    
     - Tries to use the performance API where possible.
    
    - Address discrepancies between performance API timestamps and system time.. The assumption that the clock used by Date and the clock used by the performance API  are somehow in sync is likely the reason for the generation of erroneous data.  It is very likely that there is a significant clock skew found between the monotonic clock used by the performance API and current state of the system clock. There appears to be a lot of nuance here, and the exact way this plays out depends on the OS, browser, and browser version, and if the machine has been put into sleep mode. One thing is clear, mixing the performance API timestamps and Date timestamps appears to not work very well.
    - Adds a performance fallback class that can fill in for situations where the performance api is missing.
    
    - Adds some logic around timing values that should be ignored when set to 0.
    - Prefers the permanence api’s clock when possible, since it’s resilient to skewed metrics due to a computer being put to sleep.
    
    - For browser’s that do not support the performance api, we will not produce timing data.
    - For browser’s that do not support the performance api, we will  make a best effort to produce timing data; however, if we detect the machine enters sleep mode during data collection, the data will be deemed unreliable and will not be recorded.

--- packages/fxa-shared/speed-trap/navigation-timing.js
+++ packages/fxa-shared/speed-trap/navigation-timing.js
@@ -2,66 +2,60 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const NAVIGATION_TIMING_FIELDS = {
-  navigationStart: undefined,
-  unloadEventStart: undefined,
-  unloadEventEnd: undefined,
-  redirectStart: undefined,
-  redirectEnd: undefined,
-  fetchStart: undefined,
-  domainLookupStart: undefined,
-  domainLookupEnd: undefined,
-  connectStart: undefined,
-  connectEnd: undefined,
-  secureConnectionStart: undefined,
-  requestStart: undefined,
-  responseStart: undefined,
-  responseEnd: undefined,
-  domLoading: undefined,
-  domInteractive: undefined,
-  domContentLoadedEventStart: undefined,
-  domContentLoadedEventEnd: undefined,
-  domComplete: undefined,
-  loadEventStart: undefined,
-  loadEventEnd: undefined,
-};
+import {NAVIGATION_TIMING_FIELDS, OPTIONAL_NAVIGATION_TIMING_FIELDS} from './timing-fields';
 
-var navigationTiming;
-try {
-  // eslint-disable-next-line no-undef
-  navigationTiming = window.performance.timing;
-} catch (e) {
-  // NOOP
-}
+// eslint-disable-next-line
+const startDateNow = Date.now();
+// eslint-disable-next-line
+const startPerfNow = window.performance.now() + window.performance.timeOrigin;
 
-if (!navigationTiming) {
-  navigationTiming = Object.create(NAVIGATION_TIMING_FIELDS);
-}
-
-var navigationStart = navigationTiming.navigationStart || Date.now();
 
 class NavigationTiming {
-  init(options) {
-    options = options || {};
-    this.navigationTiming = options.navigationTiming || navigationTiming;
-    this.baseTime = navigationStart;
+  init(opts) {
+
+    // A performance api must be provided
+    if (!opts || !opts.performance) {
+      throw new Error('opts.performance is required!')
+    }
+    this.performance = opts.performance;
   }
 
   get() {
-    return this.navigationTiming;
+    return this.performance.timing;
   }
 
   diff() {
-    var diff = {};
-    var baseTime = this.baseTime;
 
-    for (var key in NAVIGATION_TIMING_FIELDS) {
-      var timing = this.navigationTiming[key];
+    // If we are using our fallback performance api (ie window.performance
+    // doesn't  exist), don't return anything.
+    if (this.performance.unreliable === true) {
+      return undefined;
+    }
 
-      if (timing >= baseTime) {
-        diff[key] = timing - baseTime;
-      } else {
+    // Make navigation timings relative to navigation start.
+    const diff = {};
+    for (const key in NAVIGATION_TIMING_FIELDS) {
+      const timing = this.performance.timing[key];
+
+      // Compute the delta relative to navigation start. This removes any
+      // ambiguity around what the 'start' or 'baseTime' time is. Since we
+      // are sure the current set of navigation timings were created using
+      // the same kind of clock, this seems like the safest way to do this.
+      const delta = timing - this.performance.timing.navigationStart;
+
+      if (timing === 0 && OPTIONAL_NAVIGATION_TIMING_FIELDS.indexOf(key) >= 0) {
+        // A time value of 0 for certain fields indicates a non-applicable value. Set to null.
         diff[key] = null;
+      }
+      else if (delta < 0) {
+        // We shouldn't see any negative values. If we do something went very wrong.
+        // We will set use -11111 as a magic number to ensure a sentry error is captured,
+        // and it's easy to spot
+        diff[key] = -11111;
+      }
+      else {
+        // The expected 'happy path'
+        diff[key] = delta;
       }
     }
     return diff;
